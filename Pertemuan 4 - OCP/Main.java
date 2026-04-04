5584d5a75c4ccd54157fa0df7790e3842a6472c8
hamizan18 <hamizanputra321@gmail.com>
2026-04-04 17:47:45 +0700

    pasue vct bentra

--- Pertemuan 4 - OCP/Main.java
+++ Pertemuan 4 - OCP/Main.java
@@ -10,27 +10,33 @@ public class Main {
 
         int discount = random.nextInt(max - min + 1) + 2; // biar diskonnya 2 - 15%
         
-        System.out.println("Produk hari ini memiliki diskon sebesar " + discount + "% untuk total pembelian!!");
-        System.out.print("Masukkan nama produk: ");
-        String product = scanner.nextLine();
-        
-        System.out.print("Masukkan Jumlah: ");
-        int quantity = scanner.nextInt();
-        
-        System.out.print("Masukkan harga satuan: ");
-        double price = scanner.nextDouble();
+        while True:
+            try:
 
-        OrderManager order = new OrderManager();
-        order.setProduct(product);
-        order.setPrice(price);
-        order.setQuantity(quantity);
-        order.setDiscount(discount);
-        
-        SaveOrder saveOrder = new SaveOrder(order);
-        saveOrder.saveOrder();
+                System.out.println("Produk hari ini memiliki diskon sebesar " + discount + "% untuk total pembelian!!");
+                System.out.print("Masukkan nama produk: ");
+                String product = scanner.nextLine();
+                
+                System.out.print("Masukkan Jumlah: ");
+                int quantity = scanner.nextInt();
+                
+                System.out.print("Masukkan harga satuan: ");
+                double price = scanner.nextDouble();
 
-        PrintReceipt printer = new PrintReceipt(order);
-        printer.printReceipt();
+                OrderManager order = new OrderManager();
+                order.setProduct(product);
+                order.setPrice(price);
+                order.setQuantity(quantity);
+                order.setDiscount(discount);
+                
+                SaveOrder saveOrder = new SaveOrder(order);
+                saveOrder.saveOrder();
+
+                PrintReceipt printer = new PrintReceipt(order);
+                printer.printReceipt();
+            
+            except:
+            System.out.println("Mohon input 'Y' or 'N'..");
 
         scanner.close();
     }
