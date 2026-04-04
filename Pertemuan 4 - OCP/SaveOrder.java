607f3e026892fc9e0ec7b4a84e95d403da991163
hamizan18 <hamizanputra321@gmail.com>
2026-04-04 18:22:10 +0700

    rapihin dikit bagian saveorder

--- Pertemuan 4 - OCP/SaveOrder.java
+++ Pertemuan 4 - OCP/SaveOrder.java
@@ -15,7 +15,7 @@ public class SaveOrder {
         double discount = takeOrder.getDiscount();
 
         try (FileWriter writer = new FileWriter("orders.txt", true)) {
-            writer.write("Diskon: " + discount + " - " + product + " - " + quantity + " - Rp" + total + "\n");
+            writer.write("Diskon " + discount + "%" + " - " + product + " - " + quantity + " - Rp" + total + "\n");
             System.out.println("Pesanan telah disimpan!");
         } catch (IOException e) {
             System.out.println("Gagal menyimpan pesanan" + e.getMessage());
