c7308c8eac44175f01dd57587c4358d2677eccd2
hamizan18 <hamizanputra321@gmail.com>
2026-04-04 18:13:31 +0700

    phase 2 clear

--- Pertemuan 4 - OCP/Main.java
+++ Pertemuan 4 - OCP/Main.java
@@ -7,37 +7,49 @@ public class Main {
         Random random = new Random();
         int max = 15;
         int min = 2;
+        char validate = 'Y';
 
         int discount = random.nextInt(max - min + 1) + 2; // biar diskonnya 2 - 15%
         
-        while True:
-            try:
+        while (true){
 
-                System.out.println("Produk hari ini memiliki diskon sebesar " + discount + "% untuk total pembelian!!");
-                System.out.print("Masukkan nama produk: ");
-                String product = scanner.nextLine();
-                
-                System.out.print("Masukkan Jumlah: ");
-                int quantity = scanner.nextInt();
-                
-                System.out.print("Masukkan harga satuan: ");
-                double price = scanner.nextDouble();
+            try {
+                if (validate == 'Y' || validate == 'y') {
+                    System.out.println("Produk hari ini memiliki diskon sebesar " + discount + "% untuk total pembelian!!");
+                    System.out.print("Masukkan nama produk: ");
+                    String product = scanner.nextLine();
+                    
+                    System.out.print("Masukkan Jumlah: ");
+                    int quantity = scanner.nextInt();
+                    
+                    System.out.print("Masukkan harga satuan: ");
+                    double price = scanner.nextDouble();
 
-                OrderManager order = new OrderManager();
-                order.setProduct(product);
-                order.setPrice(price);
-                order.setQuantity(quantity);
-                order.setDiscount(discount);
-                
-                SaveOrder saveOrder = new SaveOrder(order);
-                saveOrder.saveOrder();
+                    scanner.nextLine(); // ntuk buang newline
 
-                PrintReceipt printer = new PrintReceipt(order);
-                printer.printReceipt();
-            
-            except:
-            System.out.println("Mohon input 'Y' or 'N'..");
+                    OrderManager order = new OrderManager();
+                    order.setProduct(product);
+                    order.setPrice(price);
+                    order.setQuantity(quantity);
+                    order.setDiscount(discount);
+                    
+                    SaveOrder saveOrder = new SaveOrder(order);
+                    saveOrder.saveOrder();
 
+                    PrintReceipt printer = new PrintReceipt(order);
+                    printer.printReceipt();
+                    
+                    System.out.println("Order lagi? (Y/N): ");
+                    validate = scanner.next().charAt(0); // ni ntuk ambil Char bukan String
+                } else if (validate == 'N' || validate == 'n') {
+                    break;
+                } else {
+                    System.out.println("Mohon input 'Y' atau 'N'");
+                    System.out.println("Order lagi? (Y/N): ");
+                    validate = scanner.next().charAt(0);
+                }
+            }
+        }
         scanner.close();
     }
 }
