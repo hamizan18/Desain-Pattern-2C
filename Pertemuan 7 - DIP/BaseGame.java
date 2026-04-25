64cfd3945538f26925f0676620788c2d9a6bbc4b
hamizan18 <hamizanputra321@gmail.com>
2026-04-26 02:12:04 +0700

    rest

--- Pertemuan 7 - DIP/BaseGame.java
+++ Pertemuan 7 - DIP/BaseGame.java
@@ -8,8 +8,27 @@ public class BaseGame {
         xbox = new XboxController();
         ps = new PSController();
 
+        // tambah pembagian nintendo, xbox, dan ps
         nintendo.AButton();
+        nintendo.BButton();
+        nintendo.XButton();
+        nintendo.YButton();
+        nintendo.leftAnalog();
+        nintendo.rightAnalog();
+        nintendo.homeButton();
         xbox.AButton();
+        xbox.BButton();
+        xbox.XButton();
+        xbox.YButton();
+        xbox.leftAnalog();
+        xbox.rightAnalog();
+        xbox.homeButton();
         ps.AButton();
+        ps.BButton();
+        ps.XButton();
+        ps.YButton();
+        ps.leftAnalog();
+        ps.rightAnalog();
+        ps.homeButton();
     }
 }
